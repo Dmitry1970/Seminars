8f7c18c6a7ed6ce3858d4bc701865d4146ca6209
Dmitry <badl1970@mail.ru>
2022-10-09 17:47:15 +0300

    решение задач на семинаре

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -10,15 +10,15 @@ Console.Write("Y: ");
 int y = Convert.ToInt32(Console.ReadLine());
 
 int quarter = Quarter(x, y);
-string result = quarter > 0 ? $"Указанные координаты соответствуют четверти -> {quarter}" : 
-                              "Введены некорректные координаты";  
+string result = quarter > 0 ? $"Указанные координаты соответствуют четверти -> {quarter}" :
+                              "Введены некорректные координаты";
 Console.Write(result);
 
 int Quarter(int xc, int yc)
 {
-    if(xc > 0 && yc > 0) return 1;
-    if(xc < 0 && yc > 0) return 2;
-    if(xc < 0 && yc < 0) return 3;
-    if(xc > 0 && yc < 0) return 4;
+    if (xc > 0 && yc > 0) return 1;
+    if (xc < 0 && yc > 0) return 2;
+    if (xc < 0 && yc < 0) return 3;
+    if (xc > 0 && yc < 0) return 4;
     return 0;
 }
