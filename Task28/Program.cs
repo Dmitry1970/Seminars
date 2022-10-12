ac61a466d33b5378e3c09005ed6e5486c2cb7f3a
Dmitry <badl1970@mail.ru>
2022-10-12 17:11:53 +0300

    решение задачи на семинаре

---
+++ Task28/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 28: Напишите программу, которая
+// 1. принимает на вход число N и 
+// 2. выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = Factorial(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {factorial}");
+
+
+int Factorial(int num)
+{
+    int count = 1;
+    int i;
+    for (i = 1; i <= num; i++)
+    {
+        try
+        {
+            checked   // проверка на переполнение
+            {
+                count = count * i;
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Переполнение типа!");
+            break;
+        }
+
+    }
+    Console.WriteLine($"Факториал числа {i - 1} = {count}");
+    return count;
+}
