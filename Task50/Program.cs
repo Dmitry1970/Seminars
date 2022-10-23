59f6ac42c7588987a6db0451723d5a552f09f385
Dmitry <badl1970@mail.ru>
2022-10-23 17:35:17 +0300

    fixed bugs

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -17,9 +17,9 @@ int rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов в массиве:");
 int columns = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите номер строки:");
+Console.WriteLine("Введите индекс элемента в строке:");
 int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите номер столбца:");
+Console.WriteLine("Введите индекс элемента в столбце:");
 int y = Convert.ToInt32(Console.ReadLine());
 
 int[,] CreateMatrix(int a, int b, int min, int max)
@@ -28,17 +28,21 @@ int[,] CreateMatrix(int a, int b, int min, int max)
     Random rnd = new Random();
     for (int i = 0; i < myMatrix.GetLength(0); i++)
     {
-        for (int j = 0; j < myMatrix.GetLength(1); j++)
+        if (a > 0 && b > 0)
         {
-            myMatrix[i, j] = rnd.Next(min, max);
+            for (int j = 0; j < myMatrix.GetLength(1); j++)
+            {
+                myMatrix[i, j] = rnd.Next(min, max);
+            }
         }
+
     }
     return myMatrix;
 }
 
 bool isElement(int[,] matrix)
 {
-    return x <= rows && y <= columns;
+    return x < rows && x >= 0 && y < columns && y >= 0;
 }
 
 void PrintMatrix(int[,] array2d)
@@ -56,8 +60,19 @@ void PrintMatrix(int[,] array2d)
     }
 }
 
-int[,] newArray2d = CreateMatrix(rows, columns, 0, 10);
-PrintMatrix(newArray2d);
-Console.WriteLine();
-if (isElement(newArray2d)) Console.WriteLine($"{x}, {y} -> такой элемент в массиве есть");
-else Console.WriteLine($"{x}, {y} -> такого элемента в массиве нет");
+if (rows > 0 && columns > 0)
+{
+    int[,] newArray2d = CreateMatrix(rows, columns, 0, 10);
+    PrintMatrix(newArray2d);
+    Console.WriteLine();
+    bool c = isElement(newArray2d);
+    if (c) Console.WriteLine($"{x}, {y} -> такой элемент в массиве есть -> {newArray2d[x, y]}");
+    else if (x < 0 && y >= 0 && y < columns) Console.WriteLine("Такого элемента в массиве нет. Введите значение индекса строки больше 0.");
+    else if (x >= 0 && x < rows && y < 0) Console.WriteLine("Такого элемента в массиве нет. Введите значение индекса столбца больше 0.");
+    else if (x < 0 && y < 0) Console.WriteLine("Такого элемента в массиве нет. Введите значение индекса строки и столбца больше 0.");
+    else Console.WriteLine($"{x}, {y} -> такого элемента в массиве нет");
+}
+else if (rows < 0 && columns > 0) Console.WriteLine("Введите значение строки больше 0.");
+else if (rows > 0 && columns < 0) Console.WriteLine("Введите значение столбца больше 0.");
+else Console.WriteLine("Введите значение строки и столбца больше 0.");
+
