549465bf8155c5515d8525a26cce93e1697ae3ba
Dmitry <badl1970@mail.ru>
2022-11-15 17:34:22 +0300

    Task60 is done

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -13,18 +13,26 @@ Console.Write("Введите второе измерение: ");
 int secondDim = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье измерение: ");
 int thirdDim = Convert.ToInt32(Console.ReadLine());
+if (firstDim * secondDim * thirdDim > 99)
+{
+    Console.WriteLine("Превышен предел значений элементов по условию задачи");
+    return;
+}
 CreateThreeDimesionMatrix(firstDim, secondDim, thirdDim);
 int numbers = firstDim * secondDim * thirdDim;
-int[] arr = FillArray(numbers);
-PrintArray(arr);
-
-
 
 
 int[,,] CreateThreeDimesionMatrix(int row, int column, int depth)
 {
     int[,,] matrix = new int[row, column, depth];
     int numbers = row * column * depth;
+    int arrayIndex = 0;
+    int startNumber = 10;
+    int[] myArray = new int[numbers];
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        myArray[i] = startNumber++;
+    }
 
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
@@ -32,6 +40,7 @@ int[,,] CreateThreeDimesionMatrix(int row, int column, int depth)
         {
             for (int k = 0; k < matrix.GetLength(2); k++)
             {
+                matrix[i, j, k] = myArray[arrayIndex++];
                 Console.Write($" {matrix[i, j, k]}({i},{j},{k}) ");
             }
             Console.WriteLine();
@@ -39,32 +48,4 @@ int[,,] CreateThreeDimesionMatrix(int row, int column, int depth)
         Console.WriteLine();
     }
     return matrix;
-}
-
-int[] FillArray(int numb)
-{
-    int[] array = new int[numb];
-    int startNumber = 10;
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = startNumber++;
-    }
-    return array;
-}
-
-void PrintArray(int[] array)
-{
-    Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (i < array.Length - 1)
-        {
-            Console.Write($"{array[i],2}, ");
-        }
-        else
-        {
-            Console.Write($"{array[i],2}");
-        }
-    }
-    Console.WriteLine("]");
 }
