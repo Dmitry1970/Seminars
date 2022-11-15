5d27a8f1dc57af773e96d8ddaa44c05c3c49c4cb
Dmitry <badl1970@mail.ru>
2022-11-15 21:47:18 +0300

    Task66 is done

---
+++ Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Практическое задание 9
+
+// Задайте значения М и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+
+Console.Write("Введите первое значение: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе значение: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+int SumOfNaturalElements(int startNumber, int finishNumber)
+{
+    if (finishNumber == startNumber) return startNumber;
+    if (startNumber < finishNumber)
+    {
+        return startNumber + SumOfNaturalElements(startNumber + 1, finishNumber);
+    }
+    return startNumber + SumOfNaturalElements(startNumber - 1, finishNumber);
+}
+
+int result = SumOfNaturalElements(number1, number2);
+Console.WriteLine(result);
